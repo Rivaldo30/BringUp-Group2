c1c8ee1de0480a68b7cfecfbec775ebdd25ede95
badriamujki <deathlige@gmail.com>
2021-10-29 10:44:07 +0800

    Tambah+

---
+++ app/src/main/java/id/ac/uversrivaldo/bringup/Tampilan_Jawaban.java
@@ -0,0 +1,14 @@
+package id.ac.uversrivaldo.bringup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Tampilan_Jawaban extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tampilan_jawaban);
+    }
+}
