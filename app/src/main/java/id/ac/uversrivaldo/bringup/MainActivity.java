eb09201a0907b2ad0b77144b2637b165d8a0ae4f
badriamujki <deathlige@gmail.com>
2021-11-01 02:30:14 +0800

    add

--- app/src/main/java/id/ac/uversrivaldo/bringup/MainActivity.java
+++ app/src/main/java/id/ac/uversrivaldo/bringup/MainActivity.java
@@ -1,14 +1,45 @@
 package id.ac.uversrivaldo.bringup;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView rvSoal;
+    private final ArrayList<Soal> list = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        rvSoal = findViewById(R.id.recycle);
+        rvSoal.setHasFixedSize(true);
+
+
+        list.addAll(getListSoal());
+        showRecyclerList();
+    }
+    public ArrayList<Soal> getListSoal() {
+        String[] dataSoal = getResources().getStringArray(R.array.data_soal);
+
+
+        ArrayList<Soal> listSoal= new ArrayList<>();
+        for (int i = 0; i < dataSoal.length; i++) {
+            Soal soal = new Soal();
+            soal.setSoal(dataSoal[i]);
+            listSoal.add(soal);
+        }
+        return listSoal;
+    }
+
+    private void showRecyclerList(){
+        rvSoal.setLayoutManager(new LinearLayoutManager(this));
+        ListSoalAdapter listSoalAdapter = new ListSoalAdapter(list);
+        rvSoal.setAdapter(listSoalAdapter);
     }
 }
---
+++ app/src/main/java/id/ac/uversrivaldo/bringup/Soal.java
@@ -0,0 +1,56 @@
+package id.ac.uversrivaldo.bringup;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Soal implements Parcelable {
+    private String soal;
+    private String jawaban;
+
+    Soal(){
+
+    }
+    protected Soal(Parcel in) {
+        soal = in.readString();
+        jawaban = in.readString();
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(soal);
+        dest.writeString(jawaban);
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    public static final Creator<Soal> CREATOR = new Creator<Soal>() {
+        @Override
+        public Soal createFromParcel(Parcel in) {
+            return new Soal(in);
+        }
+
+        @Override
+        public Soal[] newArray(int size) {
+            return new Soal[size];
+        }
+    };
+
+    public String getSoal() {
+        return soal;
+    }
+
+    public void setSoal(String soal) {
+        this.soal = soal;
+    }
+
+    public String getJawaban() {
+        return jawaban;
+    }
+
+    public void setJawaban(String jawaban) {
+        this.jawaban = jawaban;
+    }
+}
